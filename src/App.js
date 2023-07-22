f6096ff2e5eafb1391a8e9c25db89747021357cd
Biswajit <biswajitghosh150803@gmail.com>
2023-07-22 10:36:22 +0530

    ...

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { GoogleOAuthProvider,GoogleLogin } from '@react-oauth/google';
+import jwt_decode from "jwt-decode";
+import { useState } from 'react';
 function App() {
+  const [pic,setPic]=useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
+    Hi,I am Biswajit Ghosh&nbsp;&nbsp;
+    <img src={pic} alt="pic" style={{width:"50px",height:"50px",borderRadius:"50%"}}/>
+    <GoogleOAuthProvider clientId="524025551006-t4fhvf3hj02cqgqilp8492muc4usd8sc.apps.googleusercontent.com">
+    <GoogleLogin
+  onSuccess={credentialResponse => {
+    var decoded = jwt_decode(credentialResponse.credential);
+    console.log(decoded);
+    setPic(decoded.picture);
+    console.log(pic)
+  }}
+  onError={() => {
+    console.log('Login Failed');
+  }}
+/>
+    </GoogleOAuthProvider>
     </div>
   );
 }
